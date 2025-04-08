f57521c3f769dfb33b0443b878206c5ba16ab36d
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-04-08 20:00:27 +0100

    Add JobOperation and ScheduledOperation model classes to represent operations

---
+++ JobShopScheduler/Models/ScheduledOperation.cs
@@ -0,0 +1,12 @@
+﻿namespace JobShopScheduler.Models
+{
+    public class ScheduledOperation
+    {
+        public int JobId { get; set; }
+        public int OperationId { get; set; }
+        public string Subdivision { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime => StartTime + ProcessingTime;
+        public int ProcessingTime { get; set; }
+    }
+}
