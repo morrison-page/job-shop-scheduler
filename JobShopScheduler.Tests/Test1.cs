7f1cdf0c7c190cb987db7c7f929c3fabddccd241
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-04-08 19:24:36 +0100

    Initial commit

---
+++ JobShopScheduler.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace JobShopScheduler.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Job Shop Scheduler
+
+The Job Shop Scheduler (JSS) optimises manufacturing schedules to minimise time waste by maximising efficient use of equipment.
