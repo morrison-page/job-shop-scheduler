b6f5232099aeb47bf9c25729bf1baf959deba1d1
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-04-08 20:02:54 +0100

    Add JobOperation data-retreival from CSV files in Program.cs

--- JobShopScheduler/Program.cs
+++ JobShopScheduler/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace JobShopScheduler
+﻿using JobShopScheduler.Models;
+
+namespace JobShopScheduler
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<JobOperation> operations = GetOperations();
+        }
+
+        public static List<JobOperation> GetOperations()
+        {
+            string dataSetPath = "../../../Jobs/jobs_small.csv";
+            string[] lines = File.ReadAllLines(dataSetPath);
+
+            List<JobOperation> operations = lines.Skip(1)
+                .Select(line => line.Split(','))
+                .Select(values => new JobOperation
+                {
+                    JobId = int.Parse(values[0]),
+                    OperationId = int.Parse(values[1]),
+                    Subdivision = values[2],
+                    ProcessingTime = int.Parse(values[3])
+                })
+                .ToList();
+
+            return operations;
         }
     }
 }
