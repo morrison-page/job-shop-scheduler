cbdfebb95f745e03ebcf98a2edc0e7a60ff42ed0
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-04-20 22:14:55 +0100

    Add JobReaderTests class with initial test methods

---
+++ JobShopScheduler.Tests/Helpers/JobReaderTests.cs
@@ -0,0 +1,46 @@
+﻿namespace JobShopScheduler.Tests.Helpers
+{
+    [TestClass]
+    public sealed class JobReaderTests
+    {
+        // Valid CSV & Valid Data
+        [TestMethod]
+        public void ReadJobs_ValidInput_ReturnsExpectedJobs()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Missing CSV
+        [TestMethod]
+        public void ReadJobs_MissingCsvFile_ReturnsError()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Invalid CSV
+        [TestMethod]
+        public void ReadJobs_MissingHeader_ReturnsError()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ReadJobs_MissingColumns_ReturnsError()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Valid CSV & Invalid Data
+        [TestMethod]
+        public void ReadJobs_MissingRandomData_ReturnsError()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ReadJobs_InvalidData_ReturnsError()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
