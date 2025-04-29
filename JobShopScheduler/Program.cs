a7b29b7e4a617c8082b4aa8ed3e5f5910d521b75
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-04-29 22:23:59 +0100

    Update by increase iterations for schedule comparison to always get best results in larger datasets

--- JobShopScheduler/Program.cs
+++ JobShopScheduler/Program.cs
@@ -13,24 +13,22 @@ namespace JobShopScheduler
     {
         static void Main(string[] args)
         {
-            string filePath = "../../../Jobs/jobs_pleaseStop.csv";
+            string filePath = "../../../Jobs/jobs_huge.csv";
             List<Job> jobs = JobReader.ReadJobs(filePath);
 
             Stopwatch sw = new();
-            
             IScheduleSolver solver = new GeneticAlgorithm(jobs);
+
             sw.Start();
             Schedule schedule = solver.Solve();
-            for (int i = 0; i < 10; i++)
-            { 
+            for (int i = 0; i < 40; i++)
+            {
                 Schedule compareSchedule = solver.Solve();
                 if (compareSchedule.Fitness < schedule.Fitness)
-                {
                     schedule = compareSchedule;
-                }
             }
             sw.Stop();
-
+            
             IScheduleExporter exporter = new ConsoleExporter();
             exporter.Export(schedule, jobs);
             Console.WriteLine(sw.ElapsedMilliseconds.ToString());
