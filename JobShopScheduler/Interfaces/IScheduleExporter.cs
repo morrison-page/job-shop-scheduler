da65baa84818c506639e36d4875e433a91bbecac
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-05-04 14:45:26 +0100

    Add XML comments to Interfaces

--- JobShopScheduler/Interfaces/IScheduleExporter.cs
+++ JobShopScheduler/Interfaces/IScheduleExporter.cs
@@ -1,10 +1,16 @@
 ﻿using JobShopScheduler.Models;
-using System.Collections.Generic;
 
 namespace JobShopScheduler.Interfaces
 {
+    /// <summary>
+    /// Defines a contract for exporting a schedule.
+    /// </summary>
     internal interface IScheduleExporter
     {
+        /// <summary>
+        /// Exports the provided schedule to a specific output format or destination.
+        /// </summary>
+        /// <param name="schedule">The schedule to be exported, containing evaluated jobs.</param>
         void Export(Schedule schedule);
     }
 }
--- JobShopScheduler/Interfaces/IScheduleSolver.cs
+++ JobShopScheduler/Interfaces/IScheduleSolver.cs
@@ -2,8 +2,15 @@
 
 namespace JobShopScheduler.Interfaces
 {
+    /// <summary>
+    /// Defines a contract for solving the scheduling problem and generating a schedule.
+    /// </summary>
     internal interface IScheduleSolver
     {
+        /// <summary>
+        /// Solves the scheduling problem and returns the resulting <see cref="Schedule"/> with evaluated jobs.
+        /// </summary>
+        /// <returns>A <see cref="Schedule"/> object representing the solution to the scheduling problem.</returns>
         Schedule Solve();
     }
 }
